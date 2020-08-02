2dcec69254ddca719d60bcc3342ff2a038401781
Suraj Kumar <suraj410401@gmail.com>
2020-08-02 15:21:25 +0530

    :large_blue_diamond:

--- test/transferTx.js
+++ test/transferTx.js
@@ -2,7 +2,7 @@ const pNode = "waves private node seed with waves tokens"
 const accA = " "
 
 console.log("address with waves:");
-console.log(address(node));
+console.log(address(pNode));
 
 it('transfer', async function(){
     let tx = await broadcast(transfer({amount: 1000000000, recipient: accA}, pNode))
